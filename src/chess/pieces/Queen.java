16405662efc4311d32c3976c85c4cdc5b2e271af
Inhanga <nakappa1@gmail.com>
2020-05-12 01:04:21 -0300

    Special move - Pomotion

--- src/chess/pieces/Queen.java
+++ src/chess/pieces/Queen.java
@@ -51,6 +51,7 @@ public class Queen extends ChessPiece{
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setColumn(p.getColumn() + 1);
 		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) mat[p.getRow()][p.getColumn()] = true;
 		
 		// nw
 		p.setValues(position.getRow() - 1, position.getColumn() - 1);
@@ -82,7 +83,6 @@ public class Queen extends ChessPiece{
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setValues(p.getRow() + 1, p.getColumn() + 1);
 		}
-		
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) mat[p.getRow()][p.getColumn()] = true;
 		
 		return mat;
